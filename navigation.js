938ecc32bfb3482a74472a420d1f3dc81f087b11
Harrison Patton <harry.belden@gmail.com>
2023-05-01 14:33:25 -0500

    create projects page and move navigation js into its own file with a link on each html page to keep code dry

--- navigation.js
+++ navigation.js
@@ -1,8 +1,31 @@
-var sandwichButton = document.querySelector('.sandwich-button');
-var dropdown = document.querySelector('.dropdown-menu');
-var navTitle = document.querySelector('.nav__title');
+window.onload = function () {
+  const sandwichButton = document.querySelector('.sandwich-button');
+  const sandwichButtonClose = document.querySelector('.sandwich-button__close');
+  const dropdown = document.querySelector('.dropdown-menu');
+  const navContainer = document.querySelector('.nav-container');
+  const nav = document.querySelector('.nav');
+  const navTitle = document.querySelector('.nav__title');
+  let isOpen = false;
 
-sandwichButton.addEventListener('click', function () {
-  dropdown.classList.toggle('active');
-  navTitle.classList.toggle('active');
-});
+  sandwichButton.addEventListener('click', function () {
+    dropdown.classList.toggle('active');
+    navTitle.classList.toggle('hidden');
+    sandwichButton.classList.toggle('hidden');
+    sandwichButtonClose.classList.toggle('hidden');
+    navContainer.classList.toggle('margin-right');
+    nav.classList.toggle('show');
+    nav.style.alignItems = 'normal';
+    sandwichButton.style.opacity = 0;
+  });
+
+  sandwichButtonClose.addEventListener('click', function () {
+    dropdown.classList.toggle('active');
+    navTitle.classList.toggle('hidden');
+    sandwichButtonClose.classList.toggle('hidden');
+    sandwichButton.classList.toggle('hidden');
+    navContainer.classList.toggle('margin-right');
+    nav.classList.toggle('show');
+    nav.style.alignItems = 'center';
+    sandwichButton.style.opacity = 1;
+  });
+};
